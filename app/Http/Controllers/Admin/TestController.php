3dc4464b045cef82800fad46147be7acf6e2b1d8
root <root@localhost.localdomain>
2019-12-07 02:49:35 +0000

    上传框架

---
+++ app/Http/Controllers/Admin/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function save()
+    {
+    	echo "哈哈哈awfaffads";
+    }
+
+}
