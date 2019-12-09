d7cd5e882a8dbe9f979282921fc6778f5635ac02
root <root@localhost.localdomain>
2019-12-09 07:31:45 +0000

    微信接入

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 
 Route::get('save','Admin\TestController@save');
 Route::get('adduser','Admin\LoginController@adduser');
+Route::get('/wx','Weixin\WxController@wechat');
